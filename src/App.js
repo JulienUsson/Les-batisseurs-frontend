1e2ebddab7a3809a15a144c55cab9c021fb84ee0
Julien Usson <jusson@agaetis.fr>
2019-10-27 21:42:00 +0100

    :lipstick: app theme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { CssBaseline } from "@material-ui/core";
-import { ThemeProvider } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  CssBaseline,
+  Container,
+  AppBar,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
+import {
+  ThemeProvider,
+  makeStyles,
+  createStyles
+} from "@material-ui/core/styles";
 import theme from "./theme";
 import Home from "./scenes/Home";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      marginTop: theme.spacing(4)
+    }
+  })
+);
+
 function App() {
+  const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Les Bâtisseurs - Moyen-Âge</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md" className={classes.container}>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </Container>
     </ThemeProvider>
   );
 }
