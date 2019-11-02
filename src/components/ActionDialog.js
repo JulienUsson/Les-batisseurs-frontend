fef836cfcd2be2a72c67fbc8fc7774552242b67e
Julien Usson <jusson@agaetis.fr>
2019-11-02 22:33:09 +0100

    :sparkles: select action

--- src/components/ActionDialog.js
+++ src/components/ActionDialog.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Dialog from "@material-ui/core/Dialog";
+import SelectActionContent from "./SelectActionContent";
 
 function ActionDialog({ open, onClose, game }) {
-  return <Dialog fullWidth open={open} onClose={onClose}></Dialog>;
+  const [type, setType] = useState();
+
+  return (
+    <Dialog fullWidth open={open} onClose={onClose}>
+      {(function() {
+        switch (type) {
+          case "TAKE_BUILDING":
+            return null;
+          case "TAKE_WORKER":
+            return null;
+          case "SEND_WORKER":
+            return null;
+          case "TAKE_MONEY":
+            return null;
+          case "BUY_ACTION":
+            return null;
+          case "END_TURN":
+            return null;
+          default:
+            return <SelectActionContent onActionSelect={setType} />;
+        }
+      })()}
+    </Dialog>
+  );
 }
 
 export default ActionDialog;
